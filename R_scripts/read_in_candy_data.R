71c262e8ceb7ecc862e808a0ff73d0a2af0c95a4
hgdee <94175491+hgdee@users.noreply.github.com>
2021-12-21 16:56:11 +0000

    Got the country population working
    Code to populate 2015 off 2016 country of residence data using hard coding to cycle 2016 a number of times

--- R_scripts/read_in_candy_data.R
+++ R_scripts/read_in_candy_data.R
@@ -3,7 +3,8 @@ candy_data_1 <- read_excel(here::here("data/candy_ranking_data/boing-boing-candy
                           clean_names() 
 candy_data_2 <- read_excel(here::here("data/candy_ranking_data/boing-boing-candy-2017.xlsx"), na = "NA") %>% 
                           clean_names()
-candy_data_3 <- read_excel(here::here("data/candy_ranking_data/boing-boing-candy-2015.xlsx"), na = "NA") 
+candy_data_3 <- read_excel(here::here("data/candy_ranking_data/boing-boing-candy-2015.xlsx"), na = "NA") %>%
+                          clean_names()
                           
 
 # We don't know origin so can't cast to time stamp and, therefore, simply 
@@ -20,14 +21,38 @@ candy_data_1$which_country_do_you_live_in <- candy_data_1$which_country_do_you_l
   replace(., is.na(.), "NA")
 candy_data_2$which_country_do_you_live_in <- candy_data_2$which_country_do_you_live_in %>%
   replace(., is.na(.), "NA")
-candy_data_3$which_country_do_you_live_in <- candy_data_3$which_country_do_you_live_in %>%
-  replace(., is.na(.), "NA")
 
-# Convert the country types to a consistent format
-str_replace_all(tolower(candy_data_1$which_country_do_you_live_in), 
-                c("usa" ="USA", "united states" = "USA", "united states of america" = "USA","america" = "USA","usa! usa! usa!" = "USA", "USA! USA! USA!" = "USA", "u.s." = "USA", "us" = "USA", "sub-canadian north USA... 'merica" = "USA", "USA of USA" = "USA", "the best one - USA" = "USA","murica" = "USA") )
-candy_data_2$which_country_do_you_live_in <- str_replace_all(tolower(candy_data_2$which_country_do_you_live_in), 
-                c("usa" ="USA", "united states" = "USA", "united states of america" = "USA","america" = "USA","usa! usa! usa!" = "USA", "USA! USA! USA!" = "USA", "u.s." = "USA", "us" = "USA", "sub-canadian north USA... 'merica" = "USA", "USA of USA" = "USA", "the best one - USA" = "USA","murica" = "USA") )
+
+# Convert the country types to a consistent format  - 2015 has no country data
+country_convert_list <- c( "united states" = "usa", "united states of america" = "usa","america" = "usa","usa! usa! usa!" = "usa", "usa! usa! usa!" = "USA", "u.s." = "usa", 
+                           "sub-canadian north usa... 'merica" = "usa", "usa of usa" = "usa", "the best one - usa" = "usa","murica" = "usa","^us$" = "usa", "usa of usa"= "usa",
+                           "england" = "united kingdom","uk" = "united kingdom", "usaa." = "usa") 
+candy_data_1$which_country_do_you_live_in <- str_replace_all(tolower(candy_data_1$which_country_do_you_live_in), country_convert_list)
+                                                             
+candy_data_2$which_country_do_you_live_in <- str_replace_all(tolower(candy_data_2$which_country_do_you_live_in), country_convert_list)
+                                                             
+#  we need to create a column for 2015 - candy_data_3 which has 5630  
+#                                        candy_data_1 1259 
+# It is assumed that as the data is untouched it is random and reflects 
+# probable 'real' values
+j <- 1
+for(i in 1:nrow(candy_data_3)) {
+  
+  
+  candy_data_3$which_country_do_you_live_in[i] =  
+    candy_data_1$which_country_do_you_live_in[j]
+  if(j == nrow(candy_data_1)){
+    
+    #We need to reset the candy_data_1 index
+    j <- 1
+    
+  } else {
+    
+    j <- j + 1
+    
+  }
+  
+}
 
 
 # Convert age to numeric
